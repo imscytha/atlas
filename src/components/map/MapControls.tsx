3cc7d68c2a2e6349e391251b3810ad85c0f96d42
Scytha <imscytha@protonmail.com>
2025-11-01 21:11:06 +0100

    feat: revamp zoom controls

--- src/components/map/MapControls.tsx
+++ src/components/map/MapControls.tsx
@@ -1,16 +1,9 @@
-import { Button } from "../ui/button";
-import { Search } from "lucide-react";
-import { ZoomControls } from "./ZoomControls";
+import { ZoomControls } from "@/components/map/zoom-controls";
 
 export function MapControls() {
   return (
-    <div className="absolute top-2 right-2 flex flex-col gap-5">
-      <div>
-        <Button>
-          <Search />
-        </Button>
-      </div>
-      <ZoomControls />
+    <div className="absolute top-5 right-5 flex flex-col gap-5">
+      <ZoomControls className="size-10" />
     </div>
   )
 }
